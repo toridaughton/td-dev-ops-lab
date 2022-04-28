17eda0f918238141b3f5644ebf0c6566a9aba2b8
Tori Daughton <toriddaughton@gmail.com>
2022-04-28 16:04:30 -0600

    adding css local

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
+// app.get('/style', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../styles.css'))
+//     rollbar.info('css file served')
+// })
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Operating on Humility ${port}`))
