19846dfa9e913abe0178d5baf1ce9520bda38135
Tori Daughton <toriddaughton@gmail.com>
2022-04-28 16:00:34 -0600

    attaching html

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require(`express`)
 const path = require(`path`)
 
-const app = expres()
+const app = express()
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 
 const port = process.env.PORT || 5000
 
