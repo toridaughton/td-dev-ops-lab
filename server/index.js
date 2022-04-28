637ee48c71bb3df0a98c936518c4da018254e19a
Tori Daughton <toriddaughton@gmail.com>
2022-04-28 16:13:17 -0600

    rollbar?

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require(`express`)
 const path = require(`path`)
+const Rollbar = require('rollbar')
 
 const app = express()
 app.use(express.json())
 
+const rollbar = new Rollbar({
+  accessToken: '0510d0a882d045a5a0b45a3d622adaf5',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
+
 
 // app.get('/style', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../styles.css'))
